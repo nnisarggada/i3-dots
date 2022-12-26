575a056b830a71f08d7b00eb1dca6fdf671883d7
Nnisarg Gada <gadannisarg@gmail.com>
2022-12-27 01:02:24 +0530

    nvim md prev

--- .config/nvim/lua/nnisarggada/plugins-setup.lua
+++ .config/nvim/lua/nnisarggada/plugins-setup.lua
@@ -106,6 +106,24 @@ return packer.startup(function(use)
 
 	-- colorizer
 	use("norcalli/nvim-colorizer.lua")
+
+	-- install without yarn or npm
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
