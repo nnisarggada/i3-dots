bf7069d0b59865e7ae1ac3347543be535797edef
Nnisarg Gada <gadannisarg@gmail.com>
2023-03-30 22:28:26 +0530

    yes

--- mountNAS.sh
+++ mountNAS.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo mount -t cifs //192.168.29.242/Lappy-1TB /home/nnisarggada/Lappy -o username=nnisarg,password=Lonewolf654,uid=1000,gid=1000,workgroup=workgroup
-sudo mount -t cifs //192.168.29.242/ExternalHDD /home/nnisarggada/External -o username=nnisarg,password=Lonewolf654,uid=1000,gid=1000,workgroup=workgroup
+sudo mount -t cifs //192.168.1.2/Lappy-1TB /home/nnisarggada/Lappy -o username=nnisarg,password=Lonewolf@654,uid=1000,gid=1000,workgroup=workgroup
+sudo mount -t cifs //192.168.1.2/ExternalHDD /home/nnisarggada/External -o username=nnisarg,password=Lonewolf@654,uid=1000,gid=1000,workgroup=workgroup
