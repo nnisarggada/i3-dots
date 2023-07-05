241c70c0176babfa2ab5cffc65f4895fad02b4c4
nnisarggada <gadannisarg@gmail.com>
2023-07-05 20:14:11 +0530

    testing auto ohmyzsh install

--- install.sh
+++ install.sh
@@ -9,12 +9,12 @@ mkdir ~/.fonts
 mkdir ~/.local/bin
 
 cd
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
 makepkg -si
 cd
 
-yay -S autotiling bluez bluez-libs bluez-utils dunst git github-cli lxappearance papirus-icon-theme pavucontrol picom-jonaburg-git playerctl blueman qt5ct qt5-styleplugins rofi-bluetooth-git rofi-wifi-menu-git rofi-power-menu-git rofi-emoji gparted flameshot unzip copyq i3lock-fancy-git polybar rofi xorg-xinput firefox zsh alacritty pcmanfm htop neofetch brightnessctl ntfs-3g udisks2 gvfs ttf-ms-fonts bibata-cursor-theme neovim nodejs npm lua python-pillow zathura sweet-gtk-theme-dark polkit-gnome xwallpaper ttf-joypixels ttf-jetbrains-mono-nerd zathura-pdf-mupdf gtk-engine-murrine xclip python-pip python-pywal python-pywalfox mlocate zsh-syntax-highlighting zsh-autosuggestions libnotify jq
+yay -S autotiling bluez bluez-libs bluez-utils dunst git github-cli lxappearance papirus-icon-theme pavucontrol picom-jonaburg-git playerctl blueman qt5ct qt5-styleplugins rofi-bluetooth-git rofi-wifi-menu-git rofi-power-menu-git rofi-emoji gparted flameshot unzip copyq i3lock-fancy-git polybar rofi xorg-xinput firefox zsh oh-my-zsh alacritty pcmanfm htop neofetch brightnessctl ntfs-3g udisks2 gvfs ttf-ms-fonts bibata-cursor-theme neovim nodejs npm lua python-pillow zathura sweet-gtk-theme-dark polkit-gnome xwallpaper ttf-joypixels ttf-jetbrains-mono-nerd zathura-pdf-mupdf gtk-engine-murrine xclip python-pip python-pywal python-pywalfox mlocate zsh-syntax-highlighting zsh-autosuggestions libnotify jq
 
 cd
 sudo pywalfox install
@@ -23,6 +23,7 @@ sudo updatedb
 cd ~/GitRepos/BnW-i3/
 sudo cp -r .config/* ~/.config/
 sudo cp -r alacritty-color-export/ ~/
+sudo cp -r .oh-my-zsh/ ~/
 sudo cp -r .icons/* ~/.icons/
 sudo cp -r .fonts/* ~/.fonts/
 sudo cp -r .local/bin ~/.local/
@@ -34,6 +35,3 @@ sudo cp .viminfo ~/
 sudo cp .vimrc.plug ~/
 
 sudo cp lightdm/* /etc/lightdm/
-
-cd
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
