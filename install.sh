387b1197e7335b86aa319d70a80f9fefc92c1f6f
nnisarggada <gadannisarg@gmail.com>
2023-08-26 18:45:22 +0530

    fixes

--- install.sh
+++ install.sh
@@ -2,34 +2,54 @@
 
 mkdir ~/Desktop
 mkdir ~/Pictures
+mkdir ~/Pictures/Screenshots
 mkdir ~/Music
 mkdir ~/Downloads
 mkdir ~/.icons
 mkdir ~/.fonts
 mkdir ~/.local
-mkdir ~/.local/bin
 
-cd
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd
+if ! command -v yay &> /dev/null; then
+  git clone https://aur.archlinux.org/yay-bin
+  cd yay-bin
+  makepkg -si
+  cd ../
+  yay && rm -rf yay-bin
+else
+  echo "YAY already installed."
+  yay
+fi
 
-yay -S --noconfirm ncdu autotiling bluez bluez-libs bluez-utils dunst git github-cli lxappearance papirus-icon-theme pavucontrol picom-allusive playerctl blueman qt5ct qt5-styleplugins rofi-emoji rofi-power-menu-git gparted flameshot unzip zip copyq i3lock-fancy-git polybar rofi xorg-xinput firefox zsh alacritty pcmanfm htop neofetch brightnessctl ntfs-3g udisks2 gvfs ttf-ms-fonts bibata-cursor-theme neovim nodejs npm lua python-pillow zathura sweet-gtk-theme-dark polkit-gnome feh ttf-joypixels ttf-jetbrains-mono-nerd zathura-pdf-mupdf gtk-engine-murrine xclip python-pip mlocate zsh-syntax-highlighting libnotify jq acpi sxiv webapp-manager
+# git clone https://aur.archlinux.org/yay-bin.git
+# cd yay-bin
+# makepkg -si
+# cd ../
+# rm -rf yay-bin
+#
+progams=("ncdu" "autotiling" "bluez" "bluez-libs" "bluez-utils" "dunst" "git" "github-cli" "lxappearance" "papirus-icon-theme" "pavucontrol" "picom" "playerctl" "blueman" "qt5ct" "qt5-styleplugins" "rofi-emoji" "rofi-power-menu-git" "gparted" "flameshot" "unzip" "zip" "copyq" "i3lock-fancy-git" "polybar" "rofi" "xorg-xinput" "firefox" "zsh" "alacritty" "pcmanfm" "htop" "neofetch" "brightnessctl" "ntfs-3g" "udisks2" "gvfs" "ttf-ms-fonts" "bibata-cursor-theme" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "gtk-theme-windows10-dark" "polkit-gnome" "feh" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "gtk-engine-murrine" "xclip" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "acpi" "sxiv" "webapp-manager")
 
-cd ~/GitRepos/BnW-i3/
-sudo cp -r .config/* ~/.config/
-sudo cp -r .local/bin/* ~/.local/bin/
-sudo cp -r .icons/* ~/.icons/
-sudo cp -r .fonts/* ~/.fonts/
-sudo cp -r Pictures/* ~/Pictures/
-sudo cp -r Music/* ~/Music/
+for program in "${progams[@]}"; do
+  if ! yay -Q "$program" > /dev/null 2>&1; then
+    echo "Installing $program..."
+    yay -S --noconfirm "$program"
+    echo "$program installed successfully!"
+  else
+    echo "$program already installed."
+  fi
+done
+
+sudo cp -r .config ~/
+sudo cp -r .local ~/
+sudo cp -r .icons ~/
+sudo cp -r .fonts ~/
+sudo cp -r Pictures ~/
+sudo cp -r Music ~/
 sudo cp .vimrc ~/
 sudo cp .zshrc ~/
 sudo cp .viminfo ~/
 sudo cp .vimrc.plug ~/
 
-sudo cp lightdm/* /etc/lightdm/
+sudo cp -r lightdm /etc/
 sudo cp environment /etc/
 
 sudo chown $USER ~/ -R
