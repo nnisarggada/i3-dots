c6c5c76f398c15b47a8d98e349e460c0c99b5296
nnisarggada <gadannisarg@gmail.com>
2023-09-05 09:28:08 +0530

    fixes

--- install.sh
+++ install.sh
@@ -22,6 +22,8 @@ fi
 
 progams=("ncdu" "autotiling" "bluez" "bluez-libs" "bluez-utils" "dunst" "git" "github-cli" "lxappearance" "papirus-icon-theme" "pavucontrol" "picom" "playerctl" "blueman" "qt5ct" "qt5-styleplugins" "rofi-emoji" "rofi-power-menu-git" "rofi-calc" "gparted" "flameshot" "unzip" "zip" "copyq" "betterlockscreen" "polybar" "rofi" "xorg-xinput" "firefox" "zsh" "alacritty" "pcmanfm" "htop" "neofetch" "brightnessctl" "ntfs-3g" "udisks2" "udiskie" "gvfs" "ttf-ms-fonts" "bibata-cursor-theme" "neovim" "nodejs" "npm" "lua" "python-pillow" "zathura" "gtk-theme-windows10-dark" "polkit-gnome" "feh" "ttf-joypixels" "ttf-jetbrains-mono-nerd" "zathura-pdf-mupdf" "gtk-engine-murrine" "xclip" "python-pip" "mlocate" "zsh-syntax-highlighting" "libnotify" "jq" "acpi" "sxiv" "webapp-manager" "mission-center")
 
+yay -R --noconfirm i3lock
+
 for program in "${progams[@]}"; do
   if ! yay -Q "$program" > /dev/null 2>&1; then
     echo "Installing $program..."
@@ -45,6 +47,8 @@ sudo cp .vimrc.plug ~/
 
 sudo cp -r lightdm /etc/
 sudo cp environment /etc/
+
+betterlockscreen -u ~/Pictures/Wallpapers/what-will-you-say-today.jpg
 
 sudo chown $USER ~/ -R
 chsh -s /usr/bin/zsh
