033781e37a8050edd63aa06eb25913b600923b73
nnisarggada <gadannisarg@gmail.com>
2024-01-15 01:59:37 +0530

    fixed issue with vim backups

--- .config/nvim/lua/nnisarggada/set.lua
+++ .config/nvim/lua/nnisarggada/set.lua
@@ -10,7 +10,6 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 
 vim.opt.swapfile = true
-vim.opt.backup = true
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
